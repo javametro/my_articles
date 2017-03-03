780940c14bf4ff13515809879efd15b16f0a36e1
javametro <minda1949@sina.com>
2017-03-03 19:05:05 +0900

    modify 2000000000 to 2000000

--- Script/MakePartial-CFiles.cmd
+++ Script/MakePartial-CFiles.cmd
@@ -1,8 +1,8 @@
-fsutil file createnew C:\Recovery\OEM\Partial-C.swm 2000000000
-fsutil file createnew C:\Recovery\OEM\Partial-C2.swm 2000000000
-fsutil file createnew C:\Recovery\OEM\Partial-C3.swm 2000000000
-fsutil file createnew C:\Recovery\OEM\Partial-C4.swm 2000000000
-fsutil file createnew C:\Recovery\OEM\Partial-C5.swm 2000000000
-fsutil file createnew C:\Recovery\OEM\Partial-C6.swm 2000000000
+fsutil file createnew C:\Recovery\OEM\Partial-C.swm 2000000
+fsutil file createnew C:\Recovery\OEM\Partial-C2.swm 2000000
+fsutil file createnew C:\Recovery\OEM\Partial-C3.swm 2000000
+fsutil file createnew C:\Recovery\OEM\Partial-C4.swm 2000000
+fsutil file createnew C:\Recovery\OEM\Partial-C5.swm 2000000
+fsutil file createnew C:\Recovery\OEM\Partial-C6.swm 2000000
 pause
 exit
