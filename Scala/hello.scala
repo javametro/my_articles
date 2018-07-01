6d5ab307106009313e75940d6d43b3661c76469a
javametro <minda1949@sina.com>
2018-07-01 14:04:32 +0800

    new file:   Scala/hello.scala

---
+++ Scala/hello.scala
@@ -0,0 +1,23 @@
+//println("Hello, world, from a script!")
+
+//println("Hello, " + args(0) + "!")
+
+// while loop
+//var i = 0
+//while (i < args.length) {
+//	if(i != 0){
+//		print(" ")
+//	}
+//
+//	print(args(i))
+//	i += 1
+//}
+//
+//println()
+
+// functional programming
+//args.foreach(arg => println(arg))
+
+for(arg <- args){
+	println(arg)
+}
